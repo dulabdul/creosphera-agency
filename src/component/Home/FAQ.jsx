f0eeca9fb7e9cffce661ac0ad1f85f27c3eb9d0e
dulabdul <dul.abdoel21@gmail.com>
2023-12-09 08:14:42 +0800

    refactore(UI) : change accordion to native

--- src/component/Home/FAQ.jsx
+++ src/component/Home/FAQ.jsx
@@ -1,24 +1,56 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import Heading from '../Heading';
-import { Accordion } from 'flowbite-react';
 import Image from 'next/image';
+import CustomButton from '../Button';
+import { IoIosArrowDown } from 'react-icons/io';
 export default function FAQ() {
+  const [isActive, setIsActive] = useState(null);
+  console.log(isActive);
+  const handleAccordion = (index) => {
+    if (isActive === index) {
+      setIsActive(null);
+    } else {
+      setIsActive(index);
+    }
+  };
   const data = [
     {
-      title: 'Domain itu apasih',
+      title: 'Apakah Website Mahal?',
       content:
-        'Flowbite is an open-source library of interactive components built on top of Tailwind CSS including buttons, dropdowns, modals, navbars, and more.',
+        'Membuat website itu murah jika dibandingkan dengan apa yang akan didapatkan kedepannya bagi kemajuan suatu bisnis atau perusahaan. Mau bikin website dengan harga murah? kami solusinya. Mulai dari 700ribuan saja Anda sudah bisa punya website lho.',
     },
     {
-      title: 'Domain itu apasih',
+      title: 'Apakah Website Dapat Meningkatkan Penjualan?',
       content:
-        'Flowbite is an open-source library of interactive components built on top of Tailwind CSS including buttons, dropdowns, modals, navbars, and more.',
+        'Iya, website bisa bantu banget tingkatkan penjualan. Misalnya, punya website bagus bisa bikin orang lebih percaya sama bisnis kamu. Mereka juga bisa liat produk atau layanan kapan aja, 24/7 gitu, yang bisa bantu jualan terus. Plus, dengan situs web yang bagus, kamu bisa muncul di hasil pencarian internet, jadi lebih gampang ditemuin sama orang. Ada juga yang bisa langsung beli dari situ, lho.  Pokoknya, website ini juga bisa kasih data penting buat liat gimana pengunjungnya, apa yang mereka suka, dan gimana caranya biar bisa jualan lebih banyak lagi. Jadi, sebenernya, website itu bisa jadi alat yang super berguna buat bisnis.',
     },
     {
-      title: 'Domain itu apasih',
+      title: 'Apa saja data yang harus disiapkan?',
       content:
-        'Flowbite is an open-source library of interactive components built on top of Tailwind CSS including buttons, dropdowns, modals, navbars, and more.',
+        'Kami membutuhkan beberapa data Anda sebagai bahan untuk membangun website terkait informasi yang ingin Anda tampilkan pada situs Anda. Antara lain: data pengelola ( nama, alamat, nomor kontak ) company profile / profil bisnis, logo, deskripsi produk, foto / gambar produk, dan artikel yang diperlukan.',
+    },
+    {
+      title: 'Apakah terdapat panduan untuk melakukan edit konten web?',
+      content:
+        'Tentu saja! Kami akan memberi panduan dan jika Anda memiliki pertanyaan kami siap 24/7 untuk menjawab pertanyaan Anda.',
+    },
+    {
+      title:
+        'Apakah penting bagi situs web untuk responsif terhadap perangkat mobile?',
+      content:
+        'Sangat penting! Situs web responsif memastikan tampilan yang optimal di berbagai perangkat, meningkatkan pengalaman pengguna dan mendukung SEO.',
+    },
+    {
+      title:
+        'Apa manfaat dari menggunakan layanan digital marketing seperti Google Ads?',
+      content:
+        'Google Ads memungkinkan Anda untuk menjangkau audiens yang tepat dengan iklan yang relevan. Manfaatnya termasuk peningkatan visibilitas online, penargetan yang presisi, dan kontrol anggaran iklan.',
+    },
+    {
+      title: 'Bagaimana jika saya sudah punya domain dan hosting?',
+      content:
+        'Jika anda sudah mempunyai keduanya atau salah satunya, Anda bisa langsung menghubungi kami dan akan kami berikan harga yang berbeda dari harga paket kami',
     },
   ];
   return (
@@ -32,25 +64,37 @@ export default function FAQ() {
           <div className='flex flex-col gap-y-5'>
             {data.map((item, index) => {
               return (
-                <Accordion
-                  collapseAll
+                <div
                   key={index}
-                  className='bg-white border-none shadow-lg rounded-[32px] accordion_faq hover:rounded-[32px]'>
-                  <Accordion.Panel>
-                    <Accordion.Title className='text-primary font-medium'>
+                  className={`bg-white flex flex-col px-6 py-3 items-start justify-center rounded-2xl transition-all duration-500 `}>
+                  <div className='w-full flex items-center justify-between'>
+                    <CustomButton
+                      className='text-start'
+                      type='button'
+                      onClick={() => handleAccordion(index)}>
                       {item.title}
-                    </Accordion.Title>
-                    <Accordion.Content>
-                      <p className='mb-2 font-normal text-tersier text-base'>
-                        {item.content}
-                      </p>
-                    </Accordion.Content>
-                  </Accordion.Panel>
-                </Accordion>
+                    </CustomButton>
+                    <div className='flex justify-end h-[36px] w-[36px] items-center bg-cyanColor rounded-full px-2 py-1 transition-all duration-500'>
+                      <IoIosArrowDown
+                        className={`text-4xl text-white transition-all duration-500 ${
+                          isActive === index ? '-rotate-180' : ''
+                        }`}
+                      />
+                    </div>
+                  </div>
+                  <div
+                    className={`${
+                      isActive === index
+                        ? 'opacity-100 h-auto my-4'
+                        : 'opacity-0 h-0 overflow-hidden'
+                    } transition-all duration-[400] ease-in-out `}>
+                    <p>{item.content}</p>
+                  </div>
+                </div>
               );
             })}
           </div>
-          <div className='w-full h-full flex justify-center'>
+          <div className='w-full h-full max-h-[440px] flex justify-center'>
             <Image
               src='/images/faq.png'
               alt='FAQ'
