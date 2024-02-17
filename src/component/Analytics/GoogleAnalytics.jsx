761a2c73ecd22fb20c96494f828f18e7b3c8fb6d
dulabdul <dul.abdoel21@gmail.com>
2024-02-17 16:51:36 +0700

    fix : Fixing noscript tag in google tag manager

--- src/component/Analytics/GoogleAnalytics.jsx
+++ src/component/Analytics/GoogleAnalytics.jsx
@@ -14,7 +14,7 @@ export default function GoogleAnalyticsTracking({ GA_TRACKING_ID }) {
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 
-          gtag('config', '${GA_TRACKING_ID}');
+          gtag('config', ${GA_TRACKING_ID});
         `}
       </Script>
     </>
