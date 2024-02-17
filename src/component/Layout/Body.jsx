f25064a25a9e0de6838a85c51961d7039e78c422
dulabdul <dul.abdoel21@gmail.com>
2024-02-17 19:49:30 +0700

    Fix : move noscript tag to opening body tag

--- src/component/Layout/Body.jsx
+++ src/component/Layout/Body.jsx
@@ -12,6 +12,14 @@ export default function Body({ children, font }) {
       className={`${pathName === '/' ? 'bg-gradient' : ''} ${
         font.className
       } font-sans`}>
+      <noscript>
+        <iframe
+          src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM}`}
+          height='0'
+          width='0'
+          style={{ display: 'none', visibility: 'hidden' }}
+        />
+      </noscript>
       <Header />
       <main>{children}</main>
       <Footer />
